50e9d63d83e86484d77cdc4e7f07a45e87949104
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-15 15:07:49 +0800

    improve responsiveness

--- components/layout.js
+++ components/layout.js
@@ -1,23 +1,41 @@
+import React from "react";
 import Head from "next/head";
 import LeftPanel from "./leftPanel";
 import Content from "./content";
+import styles from "./layout.module.scss";
 
-export default function Layout(props) {
-  return (
-    <>
-    <Head>
-      <title>{props.title} - joulev's blog</title>
-    </Head>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <LeftPanel />
+export default class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sideBarHidden: true
+    }
+  }
+  render() {
+    return (
+      <>
+      <Head>
+        <title>{this.props.title} - joulev's blog</title>
+      </Head>
+      <div className="container">
+        <div className="row">
+          <div className={`col-md-4 ${this.state.sideBarHidden ? "d-none" : "d-block"} d-md-block`}>
+            <LeftPanel />
+          </div>
+          <div className="col-md-8">
+            <Content postPage={this.props.postPage}
+              content={this.props.postPage ? this.props.content : this.props.children}
+              data={this.props.data} />
+          </div>
         </div>
-        <div className="col-md-8">
-          <Content postPage={props.postPage} content={props.postPage ? props.content : props.children} data={props.data} />
+        <div className={styles.toggle} onClick={() => {this.setState( { sideBarHidden: !this.state.sideBarHidden } )}}>
+          <svg width="24" height="24">
+            <line x1="0" y1="6" x2="24" y2="6" />
+            <line x1="0" y1="18" x2="24" y2="18" />
+          </svg>
         </div>
       </div>
-    </div>
-    </>
-  );
+      </>
+    );
+  }
 }
--- components/leftPanel.js
+++ components/leftPanel.js
@@ -4,7 +4,7 @@ import styles from "./leftPanel.module.scss";
 
 export default function LeftPanel() {
   return (
-    <div className={`${styles.all} py-4`}>
+    <div className={`${styles.all} pt-4`}>
       <div className="text-center pb-4">
         <Image src="/avatar.jpg" width={150} height={150} alt="joulev" />
         <h3>Vu Van Dung</h3>
