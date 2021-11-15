75f373b69eafe29a0026e9e99e62e074403c8842
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-15 20:13:58 +0800

    use custom variable file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import "../styles/globals.scss";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [dark, setDark] = useState(false);
+  const changeMode = () => setDark(dark => !dark);
+  return <Component {...pageProps} dark={dark} changeMode={changeMode} />;
 }
