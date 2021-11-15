b32964d724f4fc7ed6277f495ff2538bfe860e7c
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-15 12:41:13 +0800

    change date format I'm at least not a MMDDYYYY guy

--- components/date.js
+++ components/date.js
@@ -3,5 +3,5 @@ import { parseISO, format, parse } from "date-fns";
 export default function Date(props) {
   const dateArr = props.time.split("/");
   const dateISO = dateArr[2] + dateArr[1] + dateArr[0];
-  return <time dateTime={dateISO}>{format(parseISO(dateISO), "LLLL d, yyyy")}</time>;
+  return <time dateTime={dateISO}>{format(parseISO(dateISO), "d LLLL yyyy")}</time>;
 }
