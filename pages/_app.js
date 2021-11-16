3fb052146a823b2592f38a690ed38099add3f656
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-16 15:53:20 +0800

    use localstorage for web app

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,12 @@ export default function App({ Component, pageProps }) {
   const [dark, setDark] = useState(true);
   const changeMode = () => setDark(dark => !dark);
   useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    setDark(storedTheme ? storedTheme === "dark" : true);
+  }, []);
+  useEffect(() => {
     document.body.className = dark ? "dark" : "light";
+    localStorage.setItem("theme", dark ? "dark" : "light");
   }, [dark]);
   return <Component dark={dark} changeMode={changeMode} {...pageProps} />
 }
