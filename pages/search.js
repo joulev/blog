112bc3bb930a0e26a09b8ddcec04cd41658b840b
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-16 23:32:52 +0800

    add parser and search timeout to reduce workload The search engine should only be executed if there are no new query changes in 1 second

--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,38 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import Layout from "../components/layout";
 import SearchBox from "../components/searchBox";
+import { listPosts } from "../lib/getPosts";
+import { parseSearchQuery } from "../lib/search";
 
-export default class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      query: ""
-    };
+export async function getStaticProps() {
+  const posts = listPosts();
+  return { props: { posts } };
+}
+
+function printParsedQuery(query) {
+  const parsed = parseSearchQuery(query);
+  let str = "tags: [";
+  parsed.tags.map(tag => str += `"${tag}", `);
+  str += "]\nwords: [";
+  parsed.words.map(word => str += `"${word}", `);
+  str += "]";
+  return str;
+}
+
+export default function Search(props) {
+  const [query, setQuery] = useState("");
+  const [stillTyping, setStillTyping] = useState(false);
+  const [currentTimeout, setCurrentTimeout] = useState(null);
+  const onSearchChange = query => {
+    clearTimeout(currentTimeout);
+    setQuery(query);
+    setStillTyping(true);
+    setCurrentTimeout(setTimeout(() => setStillTyping(false), 1000));
   }
-  render = () => (
-    <Layout dark={this.props.dark} changeMode={this.props.changeMode} title="Search"
-      postPage={false} data={{}} activeLink={3}>
-      <SearchBox dark={this.props.dark} onChange={q => this.setState({ query: q })} />
-      <p>{this.state.query}</p>
-    </Layout>
-  );
+  return <Layout dark={props.dark} changeMode={props.changeMode} title="Search"
+    postPage={false} data={{}} activeLink={3}>
+    <SearchBox dark={props.dark} onChange={q => onSearchChange(q)} />
+    <p>{query}</p>
+    <p>{stillTyping ? "still typing" : printParsedQuery(query)}</p>
+  </Layout>;
 }
