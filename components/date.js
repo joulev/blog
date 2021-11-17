889384c483ad9870fa5079c97b7d3f189cf4934c
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-17 19:36:12 +0800

    add UTC time attribute to <Date> component

--- components/date.js
+++ components/date.js
@@ -1,5 +1,7 @@
 import moment from "moment";
 
 export default function Date(props) {
-  return <time dateTime={props.time}>{moment(props.time).format("D MMMM y, HH:mm:ss")}</time>;
+  return <time dateTime={props.time} title={moment(props.time).toISOString(false)}>
+    {moment(props.time).format("D MMMM y, HH:mm:ss")}
+  </time>;
 }
