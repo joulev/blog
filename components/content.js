0fa2225b72d27524f9320ba9abaf7f30b0d93ea0
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-18 14:34:18 +0800

    trigger prism highlighting on content change

--- components/content.js
+++ components/content.js
@@ -1,23 +1,24 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import Date from "./date";
 import TagList from "./tagList";
 import Prism from "prismjs";
 import "prismjs/components/prism-jsx";
 
-export default class Content extends Component {
-  componentDidMount() {
+export default function Content(props) {
+  useEffect(() => {
     Prism.highlightAll();
-  }
-  render = () => this.props.postPage ? (
-    <div>
-      <div className="pb-4">
-        <h1>{this.props.data.title}</h1>
-        <div className="mt-3 fs-5 text-muted"><Date time={this.props.data.time} /></div>
-        <div className="mt-2"><TagList dark={this.props.dark} tags={this.props.data.tag} /></div>
+  }, [props.content]);
+  if (props.postPage) {
+    return (
+      <div>
+        <div className="pb-4">
+          <h1>{props.data.title}</h1>
+          <div className="mt-3 fs-5 text-muted"><Date time={props.data.time} /></div>
+          <div className="mt-2"><TagList dark={props.dark} tags={props.data.tag} /></div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: props.content }} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: this.props.content }} />
-    </div>
-  ) : (
-    <div>{this.props.content}</div>
-  );
+    );
+  }
+  return <div>{props.content}</div>;
 }
