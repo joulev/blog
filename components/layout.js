25fb9583c595ab96a0b56605623e9047e6fac3f3
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-19 14:18:10 +0800

    rewrite layout as a function with react hooks

--- components/layout.js
+++ components/layout.js
@@ -1,105 +1,92 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { CSSTransition } from "react-transition-group";
 import LeftPanel from "./leftPanel";
 import Content from "./content";
-import styles from "./layout.module.scss";
 import { getThemeClassName } from "../lib/utils";
+import styles from "./layout.module.scss";
 
-export default class Layout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sideBarHidden: true,
-      justChangedTheme: false,
-      downEnough: false
-    }
-  }
-  componentDidMount() {
-    window.addEventListener("scroll", this.onScroll)
-  }
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.onScroll)
-  }
-  onScroll = () => {
+export default function Layout(props) {
+  const [sideBarHidden, setSideBarHidden] = useState(true);
+  const [justChangedTheme, setJustChangedTheme] = useState(false);
+  const [downEnough, setDownEnough] = useState(false);
+  const changeMode = () => {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    setJustChangedTheme(screenWidth < 768);
+    props.changeMode();
+  };
+  const onScroll = () => {
     const scrollTop = window.scrollY;
-    if (this.state.downEnough) {
-      if (scrollTop < 30) this.setState({ downEnough: false });
+    if (downEnough) {
+      if (scrollTop < 30) setDownEnough(false);
     } else {
-      if (scrollTop >= 30) this.setState({ downEnough: true });
+      if (scrollTop >= 30) setDownEnough(true);
     }
-  }
-  changeSideBarState = () => {
-    this.setState({ sideBarHidden: !this.state.sideBarHidden });
-  }
-  changeMode = () => {
-    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    this.setState({ justChangedTheme: screenWidth < 768 });
-    this.props.changeMode();
-  }
-  render() {
-    return (
-      <>
-      <Head>
-        <title>{this.props.title} - joulev's blog</title>
-      </Head>
-      <div className="container">
-        <div className="row">
-          <CSSTransition in={!this.state.sideBarHidden} timeout={600}
-            classNames={{
-              enter:        styles.leftPanelEnter,
-              enterActive:  styles.leftPanelEnterActive,
-              enterDone:    styles.leftPanelEnterDone,
-              exit:         styles.leftPanelExit,
-              exitActive:   styles.leftPanelExitActive
-            }} onExit={() => this.setState({ justChangedTheme: false })}>
-            <div className={`col-md-4 ${styles.leftPanel} ${getThemeClassName(styles, this.props.dark)}
-              ${this.state.justChangedTheme ? styles.leftPanelEnterDone : ""}`}>
-              <LeftPanel dark={this.props.dark} changeMode={this.changeMode}
-                versionInfo={this.props.versionInfo} activeLink={this.props.activeLink} />
-            </div>
-          </CSSTransition>
-          <div className={`col-md-8 ${styles.contentPanel}`}>
-            <Content dark={this.props.dark} postPage={this.props.postPage}
-              content={this.props.postPage ? this.props.content : this.props.children}
-              data={this.props.data} />
-            {this.props.postPage && this.props.children ? <div>{this.props.children}</div> : null}
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  return <>
+    <Head>
+      <title>{props.title} - joulev's blog</title>
+    </Head>
+    <div className="container">
+      <div className="row">
+        <CSSTransition in={!sideBarHidden} timeout={600}
+          classNames={{
+            enter:        styles.leftPanelEnter,
+            enterActive:  styles.leftPanelEnterActive,
+            enterDone:    styles.leftPanelEnterDone,
+            exit:         styles.leftPanelExit,
+            exitActive:   styles.leftPanelExitActive
+          }} onExit={() => setJustChangedTheme(false)}>
+          <div className={`col-md-4 ${styles.leftPanel} ${getThemeClassName(styles, props.dark)}
+            ${justChangedTheme ? styles.leftPanelEnterDone : ""}`}>
+            <LeftPanel dark={props.dark} changeMode={changeMode}
+              versionInfo={props.versionInfo} activeLink={props.activeLink} />
           </div>
+        </CSSTransition>
+        <div className={`col-md-8 ${styles.contentPanel}`}>
+          <Content dark={props.dark} postPage={props.postPage}
+            content={props.postPage ? props.content : props.children}
+            data={props.data} />
+          {props.postPage && props.children ? <div>{props.children}</div> : null}
         </div>
-        <div className={`${styles.topBtns}
-          ${this.state.downEnough && this.state.sideBarHidden ? styles.downEnough : styles.notDownEnough}
-          ${styles.toggle} ${getThemeClassName(styles, this.props.dark)}`} onClick={() => this.changeSideBarState()}>
-          <CSSTransition in={!this.state.sideBarHidden} timeout={600}
-            classNames={{
-              enter:        styles.toggleEnter,
-              enterActive:  styles.toggleEnterActive,
-              enterDone:    styles.toggleEnterDone,
-              exit:         styles.toggleExit,
-              exitActive:   styles.toggleExitActive
-            }}>
-            <svg width="24" height="24">
-              <line x1="0" y1="6" x2="24" y2="6" />
-              <line x1="0" y1="18" x2="24" y2="18" />
-            </svg>
-          </CSSTransition>
-        </div>
-        <Link href="/" passHref>
-          <div className={`${styles.topBtns}
-            ${this.state.downEnough && this.state.sideBarHidden ? styles.downEnough : styles.notDownEnough}
-            ${styles.homeBtn} ${getThemeClassName(styles, this.props.dark)}`}>
-            <svg width="24" height="24">
-              <line x1= "6" y1="20" x2="18" y2="20" />
-              <line x1= "6" y1="20" x2= "6" y2= "8" />
-              <line x1="18" y1="20" x2="18" y2= "8" />
-              <line x1="12" y1= "2" x2= "3" y2="11" />
-              <line x1="12" y1= "2" x2="21" y2="11" />
-              <line x1="18" y1= "8" x2="18" y2= "0" style={{ strokeWidth: "3px" }} />
-            </svg>
-          </div>
-        </Link>
       </div>
-      </>
-    );
-  }
+      <div className={`${styles.topBtns}
+          ${downEnough && sideBarHidden ? styles.downEnough : styles.notDownEnough}
+          ${styles.toggle} ${getThemeClassName(styles, props.dark)}`}
+        onClick={() => setSideBarHidden(!sideBarHidden)}>
+        <CSSTransition in={!sideBarHidden} timeout={600}
+          classNames={{
+            enter:        styles.toggleEnter,
+            enterActive:  styles.toggleEnterActive,
+            enterDone:    styles.toggleEnterDone,
+            exit:         styles.toggleExit,
+            exitActive:   styles.toggleExitActive
+          }}>
+          <svg width="24" height="24">
+            <line x1="0" y1="6" x2="24" y2="6" />
+            <line x1="0" y1="18" x2="24" y2="18" />
+          </svg>
+        </CSSTransition>
+      </div>
+      <Link href="/" passHref>
+        <div className={`${styles.topBtns}
+          ${downEnough && sideBarHidden ? styles.downEnough : styles.notDownEnough}
+          ${styles.homeBtn} ${getThemeClassName(styles, props.dark)}`}>
+          <svg width="24" height="24">
+            <line x1= "6" y1="20" x2="18" y2="20" />
+            <line x1= "6" y1="20" x2= "6" y2= "8" />
+            <line x1="18" y1="20" x2="18" y2= "8" />
+            <line x1="12" y1= "2" x2= "3" y2="11" />
+            <line x1="12" y1= "2" x2="21" y2="11" />
+            <line x1="18" y1= "8" x2="18" y2= "0" style={{ strokeWidth: "3px" }} />
+          </svg>
+        </div>
+      </Link>
+    </div>
+  </>;
 }
