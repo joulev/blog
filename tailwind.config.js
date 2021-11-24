0442f7fe4474ac97e25fe34fce314b2ee57fab4c
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 15:04:28 +0800

    add custom font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media",
   theme: {
+    fontFamily: {
+      "sans": ['"PT Sans"'],
+      "mono": ['"Meslo LG S"'],
+    },
     extend: {
       colors: {
         gray: {
