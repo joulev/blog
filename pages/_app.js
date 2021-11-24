e4753087ad20a361e2662146452e342d0270d27b
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 19:49:37 +0800

    use localStorage to store theme

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,18 @@ import "../styles/styles.css";
 export default function App({ Component, pageProps }) {
   const [dark, setDark] = useState(false);
   useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (!storedTheme) {
+      if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
+        storedTheme = "light";
+      }
+    }
+    setDark(storedTheme ? storedTheme === "dark" : true);
+  }, []);
+  useEffect(() => {
     if (dark) document.querySelector("html").classList.add("dark");
     else document.querySelector("html").classList.remove("dark");
+    localStorage.setItem("theme", dark ? "dark" : "light");
   }, [dark]);
   return <Component dark={dark} changeMode={() => setDark(!dark)} {...pageProps} />;
 }
