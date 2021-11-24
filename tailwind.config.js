01849ed3dfa5cbf939f46197cee45a70e8e807c5
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 15:50:42 +0800

    add border to avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media",
+  darkMode: "class",
+  important: true,
   theme: {
     fontFamily: {
       "sans": ['"PT Sans"'],
