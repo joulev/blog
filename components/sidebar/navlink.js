597a53b981ce541368dcd8a7753cc92705813d76
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 22:18:11 +0800

    remove last module CSS file We don't even need it ...

--- components/sidebar/navlink.js
+++ components/sidebar/navlink.js
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import css from "./navlink.module.css";
 
 const navlinkStyles = "\
-  rounded text-center py-2 \
+  rounded text-center py-2 no-underline \
   text-gray-900 dark:text-gray-100 hover:bg-gray-300 dark:hover:bg-gray-700";
 const activeStyles = "\
   bg-primary-100 hover:bg-primary-100 dark:bg-primary-900 dark:hover:bg-primary-900 \
@@ -11,9 +10,9 @@ const activeStyles = "\
 export default function SidebarNavlink({ active }) {
   return <>
     <div className="flex flex-col gap-2 mb-5">
-      <Link href="/"      ><a className={`${navlinkStyles} ${css.navlink} ${active === 1 ? activeStyles : ""}`}>Home</a></Link>
-      <Link href="/about" ><a className={`${navlinkStyles} ${css.navlink} ${active === 2 ? activeStyles : ""}`}>About me</a></Link>
-      <Link href="/search"><a className={`${navlinkStyles} ${css.navlink} ${active === 3 ? activeStyles : ""}`}>Search</a></Link>
+      <Link href="/"      ><a className={`${navlinkStyles} ${active === 1 ? activeStyles : ""}`}>Home</a></Link>
+      <Link href="/about" ><a className={`${navlinkStyles} ${active === 2 ? activeStyles : ""}`}>About me</a></Link>
+      <Link href="/search"><a className={`${navlinkStyles} ${active === 3 ? activeStyles : ""}`}>Search</a></Link>
     </div>
   </>;
 }
