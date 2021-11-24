b922c184a858c6ce5d4991482d62177b81f9e8bf
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 19:37:08 +0800

    workaround against a bug (?) in tailwind dark mode Ref: https://github.com/tailwindlabs/tailwindcss/discussions/2917

--- components/sidebar/navlink.js
+++ components/sidebar/navlink.js
@@ -1,5 +1,7 @@
+import css from "./navlink.module.css";
+
 const navlinkStyles = "\
-  rounded text-center py-2 no-underline\
+  rounded text-center py-2\
   text-gray-900 dark:text-gray-100 hover:bg-gray-300 dark:hover:bg-gray-700";
 const activeStyles = "\
   bg-primary-100 hover:bg-primary-100 dark:bg-primary-900 dark:hover:bg-primary-900\
@@ -8,9 +10,9 @@ const activeStyles = "\
 export default function SidebarNavlink({ active }) {
   return <>
     <div className="flex flex-col gap-2 mb-5">
-      <a href="#" className={`${navlinkStyles} ${active === 1 ? activeStyles : ""}`}>Home</a>
-      <a href="#" className={`${navlinkStyles} ${active === 2 ? activeStyles : ""}`}>About me</a>
-      <a href="#" className={`${navlinkStyles} ${active === 3 ? activeStyles : ""}`}>Search</a>
+      <a href="#" className={`${navlinkStyles} ${css.navlink} ${active === 1 ? activeStyles : ""}`}>Home</a>
+      <a href="#" className={`${navlinkStyles} ${css.navlink} ${active === 2 ? activeStyles : ""}`}>About me</a>
+      <a href="#" className={`${navlinkStyles} ${css.navlink} ${active === 3 ? activeStyles : ""}`}>Search</a>
     </div>
   </>;
 }
