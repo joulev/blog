165e2eb10f51d16e94083f26399a16c2d7fd3e16
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 16:13:49 +0800

    add navbar links

--- components/sidebar/index.js
+++ components/sidebar/index.js
@@ -1,7 +1,9 @@
 import SidebarInfo from "./info";
+import SidebarNavlink from "./navlink";
 
 export default function Sidebar() {
   return <>
     <SidebarInfo />
+    <SidebarNavlink active={2} />
   </>;
 }
---
+++ components/sidebar/navlink.js
@@ -0,0 +1,11 @@
+import css from "./navlink.module.css";
+
+export default function SidebarNavlink({ active }) {
+  return <>
+    <div className="flex flex-col gap-2">
+      <a href="#" className={`${css.navlink} ${active === 1 ? css.active : ""}`}>Home</a>
+      <a href="#" className={`${css.navlink} ${active === 2 ? css.active : ""}`}>About me</a>
+      <a href="#" className={`${css.navlink} ${active === 3 ? css.active : ""}`}>Search</a>
+    </div>
+  </>;
+}
