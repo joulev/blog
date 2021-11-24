6f6b2693949c4a7ab9b540a022cf2ae0bd5d1316
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 22:14:07 +0800

    make link to tag pages nextjs link

--- components/bigTag.js
+++ components/bigTag.js
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export default function BigTag({ tag }) {
   return <>
     <div className="mb-6">
-      <a href="#" className="
-        text-4xl rounded px-3 py-1 no-underline
-        bg-primary-100 text-primary-600
-        hover:bg-primary-600 hover:text-gray-100
-        dark:bg-primary-900 dark:text-primary-400
-        dark:hover:bg-primary-400 dark:hover:text-gray-100
-      ">{tag}</a>
+      <Link href={`/tags/${tag}`}>
+        <a className="
+          text-4xl rounded px-3 py-1 no-underline
+          bg-primary-100 text-primary-600
+          hover:bg-primary-600 hover:text-gray-100
+          dark:bg-primary-900 dark:text-primary-400
+          dark:hover:bg-primary-400 dark:hover:text-gray-100
+        ">{tag}</a>
+      </Link>
     </div>
   </>;
 }
--- components/tags.js
+++ components/tags.js
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export default function Tags({ tags }) {
   return <>
     <div className="flex gap-2">
-      {tags.split(" ").map(tag => <a href="#" className="
-        rounded px-1.5 py-0.5 no-underline
-        bg-primary-100 text-primary-600
-        hover:bg-primary-600 hover:text-gray-100
-        dark:bg-primary-900 dark:text-primary-400
-        dark:hover:bg-primary-400 dark:hover:text-gray-100
-      ">{tag}</a>)}
+      {tags.split(" ").map(tag => <Link href={`/tags/${tag}`}>
+        <a className="
+          rounded px-1.5 py-0.5 no-underline
+          bg-primary-100 text-primary-600
+          hover:bg-primary-600 hover:text-gray-100
+          dark:bg-primary-900 dark:text-primary-400
+          dark:hover:bg-primary-400 dark:hover:text-gray-100
+        ">{tag}</a>
+      </Link>)}
     </div>
   </>;
 }
