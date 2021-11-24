26c7ce12356138e284f8b5d5b2553344242bd206
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-24 16:27:07 +0800

    update colour scheme The previous colour scheme was generated from existing colours in /styles/variables.scss@main, but indeed computer-generated colours are never good...

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,25 +12,25 @@ module.exports = {
       colors: {
         gray: {
           100: "#fcfcfa", // bg light
-          200: "#e1e1df", // sidebar bg light
-          300: "#c6c5c4", // sidebar btn bg light
-          400: "#abaaa9",
-          500: "#8f8e8e",
-          600: "#747373",
-          700: "#595758", // sidebar btn bg dark
-          800: "#3e3b3d", // sidebar bg dark
+          200: "#eeeeec", // sidebar bg light
+          300: "#e0e0df", // sidebar btn bg light
+          400: "#b7b6b6",
+          500: "#8d8b8c",
+          600: "#636163",
+          700: "#393639", // sidebar btn bg dark
+          800: "#2d2b2d", // sidebar bg dark
           900: "#221f22", // bg dark
         },
         primary: {
-          100: "#86e3ea",
-          200: "#6bd0db",
-          300: "#4dc4d1",
+          100: "#cfe5e6",
+          200: "#a4dadf",
+          300: "#79cfd8",
           400: "#21b1c1",
           500: "#1e9eac",
           600: "#1a8a97",
-          700: "#156f79",
-          800: "#0f5057",
-          900: "#093034",
+          700: "#1d5f68",
+          800: "#204e55",
+          900: "#223c42",
         },
         secondary: {
           light: "#e14e05",
