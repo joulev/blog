1b9bb86e6395a2865b5ed54c5188a98662693b65
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-26 01:19:50 +0800

    move preBlock to a separate component file

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,24 +1,11 @@
 import Markdown from "markdown-to-jsx";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { monokaiDark, monokaiLight } from "../../lib/monokai";
-import LinkBtn from "../../components/linkBtn";
 import Layout from "../../components/layout";
+import LinkBtn from "../../components/linkBtn";
+import PreBlock from "../../components/preBlock";
 import TagList from "../../components/tags/tagList";
 import Date from "../../components/date";
 import { listPosts, getPostContent } from "../../lib/getPosts";
 import getCommitInfo from "../../lib/getCommitInfo";
-
-// Based on https://stackoverflow.com/a/68179028/12419999
-function CodeBlock({ className, children, dark }) {
-  let lang = "text"; // default monospaced text
-  if (className && className.startsWith("lang-")) lang = className.replace("lang-", "");
-  return <SyntaxHighlighter language={lang} style={dark ? monokaiDark : monokaiLight} children={children} />;
-}
-
-function PreBlock({ children, dark, ...rest }) {
-  if ("type" in children && children ["type"] === "code") return CodeBlock({ dark, ...children.props });
-  return <pre {...rest}>{children}</pre>;
-};
 
 export function getStaticPaths() {
   const posts = listPosts();
@@ -50,10 +37,7 @@ export default function Post({ dark, changeMode, versionInfo, post }) {
         namedCodesToUnicode: { ndash: "–" },
         overrides: {
           LinkBtn: { component: LinkBtn },
-          pre: {
-            component: PreBlock,
-            props: { dark }
-          }
+          pre: { component: PreBlock, props: { dark } }
         }
       }} />
     </Layout>
