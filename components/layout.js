607f2c75828fd48c2cf47e5d16d76171c8d5b119
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-26 02:26:38 +0800

    add navi buttons

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
+import Link from "next/link";
 import Footer from "./footer";
 import Sidebar from "./sidebar";
+
+const floatingBtnStyles = "\
+  fixed p-2 z-50 bg-gray-200 dark:bg-gray-800 \
+  border border-solid rounded border-gray-400 dark:border-gray-600 \
+  cursor-pointer hover:border-gray-500 dark:hover:border-gray-500 transition";
 
 export default function Layout({ dark, changeMode, versionInfo, title, sideLink, children }) {
   return <>
@@ -26,6 +32,31 @@ export default function Layout({ dark, changeMode, versionInfo, title, sideLink,
       <div className="max-w-md sm:max-w-lg md:hidden mx-auto px-8 py-8">
         <Footer versionInfo={versionInfo} />
       </div>
+    </div>
+    <div className={`${floatingBtnStyles} top-6 right-6 md:hidden`}>
+      <svg width="24" height="24">
+        <line x1="0" y1= "6" x2="24" y2= "6" className="stroke-current stroke-2 cap-round" />
+        <line x1="0" y1="18" x2="24" y2="18" className="stroke-current stroke-2 cap-round" />
+      </svg>
+    </div>
+    <Link href="/" passHref>
+      <div className={`${floatingBtnStyles} top-6 right-20 md:hidden`}>
+        <svg width="24" height="24">
+          <line x1= "6" y1="20" x2="18" y2="20" className="stroke-current stroke-2 cap-round" />
+          <line x1= "6" y1="20" x2= "6" y2= "8" className="stroke-current stroke-2 cap-round" />
+          <line x1="18" y1="20" x2="18" y2= "8" className="stroke-current stroke-2 cap-round" />
+          <line x1="12" y1= "2" x2= "3" y2="11" className="stroke-current stroke-2 cap-round" />
+          <line x1="12" y1= "2" x2="21" y2="11" className="stroke-current stroke-2 cap-round" />
+          <line x1="18" y1= "8" x2="18" y2= "0" className="stroke-current stroke-3 cap-round" />
+        </svg>
+      </div>
+    </Link>
+    <div className={`${floatingBtnStyles} bottom-6 right-6`}>
+      <svg width="24" height="24">
+        <line x1="12" y1="2" x2="12" y2="20" className="stroke-current stroke-2 cap-round" />
+        <line x1="12" y1="2" x2= "3" y2="11" className="stroke-current stroke-2 cap-round" />
+        <line x1="12" y1="2" x2="21" y2="11" className="stroke-current stroke-2 cap-round" />
+      </svg>
     </div>
   </>;
 }
