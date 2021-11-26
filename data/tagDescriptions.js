93a255c0b852f5d1fada6c6b1bb69c404871eb3d
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-26 20:46:04 +0800

    add tag descriptions for new tags

--- data/tagDescriptions.js
+++ data/tagDescriptions.js
@@ -1,10 +1,10 @@
 const tagDescriptions = {
-  "web":
-    `All articles related to web development.`,
-  "react":
-    `All articles related to React, a JavaScript frontend framework.`,
-  "nextjs":
-    `All articles related to Next.js, a React framework by Vercel.`
+  "web": "All articles related to web development.",
+  "js": "All articles related to the JavaScript programming language.",
+  "css": "All articles related to the Cascading Style Sheets (CSS) style sheet language.",
+  "react": "All articles related to React, a JavaScript frontend framework.",
+  "nextjs": "All articles related to Next.js, a React framework by Vercel.",
+  "tailwind": "All articles related to Tailwind CSS, a CSS framework."
 };
 
 export default tagDescriptions;
