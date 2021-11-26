3c346a9062ad133eb338fb368bf29f90c54c2457
Vu Van Dung <joulev.vvd@yahoo.com>
2021-11-26 15:17:41 +0800

    add spinner to loading screen

--- components/loading.js
+++ components/loading.js
@@ -2,7 +2,10 @@ export default function Loading() {
   // always dark mode
   return <>
     <div className="fixed inset-0 flex flex-col items-center justify-center text-4xl bg-gray-900 text-gray-100">
-      loading...
+      <svg height="60" width="60" className="animate-spin">
+        <path d="M2 30 A28 28 0 0 1 58 30" className="stroke-current stroke-4 cap-round fill-none" />
+      </svg>
+      <div className="mt-5">loading…</div>
     </div>
   </>;
 }
