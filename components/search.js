e15ad97dcf403ece47fcd9626974576e5df576f0
Vu Van Dung <joulev.vvd@yahoo.com>
2021-12-03 20:12:12 +0800

    add transition effect to search box border

--- components/search.js
+++ components/search.js
@@ -6,7 +6,7 @@ export function SearchBox({ onChange, initial }) {
     <div className="relative mb-5">
       <input type="text" placeholder="Search…" onChange={(e) => onChange(e.target.value)}
         value={initial} className="
-          w-full outline-none border rounded border-solid border-gray-400 dark:border-gray-600
+          w-full outline-none transition border rounded border-solid border-gray-400 dark:border-gray-600
           placeholder-gray-500 text-xl px-4 py-2 bg-gray-200 dark:bg-gray-800
           focus:border-primary-600 dark:focus:border-primary-400 peer
         " />
